deb8905566c1cef91b6280f4d2f2973260d5becc
Yu Li <yli0607x@gmail.com>
2018-10-05 16:11:12 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,10 @@
-passengers = {
-suite_a: "Amanda Presley",
-suite_b: "Seymour Hoffman",
-suite_c: "Alfred Tennyson",
-suite_d: "Charlie Chaplin",
-suite_e: "Crumpet the Elf"
-}
+#passengers = {
+#suite_a: "Amanda Presley",
+#suite_b: "Seymour Hoffman",
+#suite_c: "Alfred Tennyson",
+#suite_d: "Charlie Chaplin",
+#suite_e: "Crumpet the Elf"
+#}
 
 def select_winner(passengers)
   # add the code snippet here!
@@ -14,4 +14,6 @@ passengers.each do |suite, name|
     winner = name
   end
 end
+
+winner
 end
